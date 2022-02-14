1041c2123ed6dffd4c09ace843768088e1cd8c71
Omran Allawy <46893512+omranellawi@users.noreply.github.com>
2022-02-14 11:18:18 +0300

    Update MyComponent.js

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,14 +1,14 @@
 import { Component, useState, tags } from "@odoo/owl";
 
 const MY_COMPONENT_TEMPLATE = tags.xml/*xml*/ `
-<span t-name="MyComponent" class="" t-on-click="update">
+<span t-name="Omran Allawy" class="" t-on-click="update">
   Hello <t t-esc="state.text"/>
 </span>
 `;
 
 export class MyComponent extends Component {
   static template = MY_COMPONENT_TEMPLATE;
-  state = useState({ text: "Owl" });
+  state = useState({ text: "All" });
   update() {
     this.state.text = this.state.text === "Owl" ? "World" : "Owl";
   }
